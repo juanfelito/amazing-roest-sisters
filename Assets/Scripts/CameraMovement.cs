ec3f6f15b59ee2ec4e7605c31e07d0c7f2327ca9
Felipe Balcero <felipe.balcero45@gmail.com>
2020-05-03 00:42:11 -0500

    First layout

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+    public Transform cameraTarget;
+    public float cameraSpeed;
+    public float minX;
+    public float minY;
+    public float maxX;
+    public float maxY;
+
+    void FixedUpdate() {
+        if(cameraTarget != null) {
+            var newPos = Vector2.Lerp(transform.position, cameraTarget.position, Time.deltaTime * cameraSpeed);
+
+            var vect3 = new Vector3(newPos.x, newPos.y, -10f);
+
+            var clampX = Mathf.Clamp(vect3.x, minX, maxX);
+            var clampY = Mathf.Clamp(vect3.y, minY, maxY);
+
+            transform.position = new Vector3(clampX, clampY, -10f);
+        }
+    }
+}
